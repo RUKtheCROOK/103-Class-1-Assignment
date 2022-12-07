85bd48ac58f11201bfca0b337312a0f2806194ab
RUKtheCROOK <johngarcia053@gmail.com>
2022-12-07 13:21:20 -0800

    Updated Script

--- script.js
+++ script.js
@@ -2,23 +2,28 @@
 // var =variable and it is not used very much
 // let =another way to make a variable with less issues and the number can be changed
 // const =constant and it is another way to make a variable but is a constant
-var age = 100;
-let userName = "JimBob";
-let taxes = 11.5;
-const name = "john";
-
-console.log(age, userName, taxes, name);
-
-userName = "RUKtheCROOK";
-
-console.log(userName);
-
+var age = 23;
+const lastName = "Garcia";
+const firstName = "John";
 let childrenNumber = 0;
-let spouseName = "Megan";
+const spouseName = "Megan";
 let geoLocation = "Nebraska";
 let Job = "Student";
+let shirtColor = "Blue";
+let pantColor = "Black";
+let computerGPU = "GeForce RTX 3070";
+let siblingsMomSide = 4;
+let siblingsDadSide = 1;
+let momsName = "Nellie";
+let dadsName = "Richard";
+let grandpasName = "Manny";
+let dogsName = "Finn";
+let currentMonth = "december";
+let currentClass = "Full Stack Dev Course";
+let futureJob = "Full Stack Dev";
+let hopefulJobStatus = "Remote";
+let furtureGeoLocation = "California";
 
 document.write(
-  `I have ${childrenNumber} children with my spouse named ${spouseName}. We live in ${geoLocation} and I currently do not work but am instead a ${Job}
-    .`
+  `I am ${age} years old and my name is ${firstName} ${lastName}. I have ${childrenNumber} kids and am married to ${spouseName}. I live in ${geoLocation} and currently do not work but I am instead a ${Job}. The shirt I am wearing is ${shirtColor} and the pants I am wearing are ${pantColor}. I have a really cool computer with a ${computerGPU} for my graphics card. I also have ${siblingsMomSide} siblings on my moms side and ${siblingsDadSide} siblings on my dads side. My moms name is ${momsName} and her dads name is ${grandpasName}. My dads name is ${dadsName} and he has been married to my mom for 15 years. I have a dog named ${dogsName} and the current month is ${currentMonth}. The class I am taking with SDGKU is a ${currentClass} and I hope to one day soon be a ${futureJob}. I really want my job status to be ${hopefulJobStatus} but I also would not mind living in ${furtureGeoLocation} becuase of how pretty it is!`
 );
